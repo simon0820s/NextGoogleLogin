eecfa82270c85bc10a28e4199a76447068a71a9e
simon0820s <simon0820s@gmail.com>
2023-09-08 20:52:58 -0500

    basic configure google provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,5 +2,8 @@ import NextAuth from "next-auth/next";
 import GoogleProvider from "next-auth/providers/google";
 
 NextAuth({
-  providers: [GoogleProvider({})],
+  providers: [GoogleProvider({
+    clientId: process.env.GOOGLE_CLIENT_ID as string,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+  })],
 });
