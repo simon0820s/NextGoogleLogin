177878ca46ef9c0c293d145b65a7d66c529a3165
simon0820s <simon0820s@gmail.com>
2023-09-12 07:31:35 -0500

    style navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -2,12 +2,13 @@ import Link from "next/link";
 
 export default function NavBar() {
   return (
-    <nav>
+    <nav className="flex justify-between px-4 font-bold py-2 shadow-sm shadow-black bg-zinc-900 text-neutral-300 text-2xl">
       <Link href='/'>
-        <h1>NextGoogle</h1>
+        <h1 className="text-amber-300">NextGoogle</h1>
       </Link>
       <div>
-        <Link href="/dashboard">Dashboard</Link>
+        <Link href="/dashboard" className="hover:text-amber-300 trasition-all ease-in-out duration-200">Dashboard</Link>
+        <button>Sigin</button>
       </div>
     </nav>
   );
